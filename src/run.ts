2f459431d0e129f4daa06bc26001d12ff1e3d30c
Chris Reeves <evschris@gmail.com>
2020-04-30 17:00:08 -0400

    commit if the delete is managing the transaction

--- src/run.ts
+++ src/run.ts
@@ -234,6 +234,7 @@ export default class ${model.name} {
       const res = await con.query(\`delete from "${model.table}" where ${model.fields.filter(f => f.pkey || f.optlock).map((f, i) => `${f.optlock ? `date_trunc('millisecond', "${f.name}")` : `"${f.name}"`} = $${i + 1}`).join(' AND ')}\`, params);
       if (res.rowCount < 1) throw new Error('No matching row to delete for ${model.name}');
       if (res.rowCount > 1) throw new Error('Too many matching rows deleted for ${model.name}');
+      if (transact) await con.commit();
     } catch (e) {
       if (transact) await con.rollback();
       throw e;
