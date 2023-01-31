0eba26bf051d08680cd5e5676a82887b5af37e4c
Chris Reeves <evschris@gmail.com>
2023-01-30 20:25:54 -0500

    overhaul connection hooks
    * add a result hook that is called any time a query is run on an
      enhanced connection
    * add result hook for connection that lives until the connection is released
    * add an enhance hook that is called any time a connection is enhanced
    * add an end hook that is called any time a connection is released
    * add an end hook for connection that lives until the connection is
      released
    * query and param info in hooks and stack traces should be more accurate
    * the rollback hook now receives the active savepoint and is called for
      savepoint rollbacks
    * a failure in savepoint rollback will trigger a full rollback
    * rollback can now accept an optional error to pass along to hooks
    * enhanced connections now have an associated id

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import * as pg from 'pg';
 
+const cfgprop = 'ts-pg-dao';
+
 /**
  * A thin wrapper on top of pg.Client that provides basic transaction support, 
  * and a convenient way to immediately execute a query with a template string.
@@ -19,7 +21,7 @@ export interface Connection extends pg.Client {
    * not be aborted. If the savepoint fails or is not supplied, the whole
    * transaction will be aborted.
    * */
-  rollback(savepoint?: SavePoint): Promise<void>;
+  rollback(savepoint?: SavePoint, err?: Error): Promise<void>;
   /** Complete the current transaction for this connection. */
   commit(): Promise<void>;
   /** Determine whether there is an active transaction for this connection. */
@@ -35,7 +37,7 @@ export interface Connection extends pg.Client {
    *
    * @returns a pg.QueryResult with any resulting rows attached
    */
-  sql<T = any>(string: TemplateStringsArray, ...parts: any[]): Promise<pg.QueryResult & { rows: T[] }>;
+  sql<T = any>(string: TemplateStringsArray, ...parts: any[]): Promise<pg.QueryResult<T>>;
   /**
    * Wrap a string such that it can be included directly in a tagged `sql` template
    * without being turned into a parameter.
@@ -46,18 +48,61 @@ export interface Connection extends pg.Client {
    * commits. If there is no current transaction, the callback will be run
    * immediately.
    * */
-  onCommit(run: () => void|Promise<void>): Promise<void>;
+  onCommit(run: ConnectionCallback): void;
   /**
    * Execute the given callback immediately after the current transaction
    * is rolled back. If there is no current transaction, the callback will
    * be discarded.
    */
-  onRollback(run: () => void|Promise<void>): Promise<void>;
-  readonly ['ts-pg-dao']: {
-    attachQueryToError?: boolean;
-    attachParametersToError?: boolean;
-  };
+  onRollback(run: RollbackCallback): void;
+
+  /**
+   * Execute the given callback immediately after any query returns a result.
+   * Result callbacks are automatically removed when a connection is released
+   * back to a pool.
+   */
+  onResult(run: ResultCallback): HookHandle;
+
+  /**
+   * Execute the given callback immediately after the connection is done. For
+   * pooled connections, this is after release, and it is after end for
+   * non-pooled connections.
+   * End callbacks are automatically removed when a connection is released
+   * back to a pool.
+   */
+  onEnd(run: EndCallback): HookHandle;
+
+  /**
+   * Enhanced connection settings and information
+   */
+  readonly [cfgprop]: EnhanceInfo;
 }
+
+export interface EnhanceInfo {
+  clientStack?: boolean;
+  attachQueryToError?: boolean;
+  attachParametersToError?: boolean;
+  readonly id: number;
+}
+
+interface InternalInfo extends EnhanceInfo {
+  __oncommit?: ConnectionCallback[];
+  __onrollback?: RollbackCallback[];
+  __onend?: EndCallback[];
+  __onresult?: ResultCallback[];
+}
+
+/**
+ * A handle to allow cancelling a listener hook.
+ */
+export interface HookHandle {
+  cancel(): void;
+}
+
+export type ConnectionCallback = (con: Connection, err?: Error) => void;
+export type RollbackCallback = (con: Connection, err?: Error, savepoint?: SavePoint) => void;
+export type ResultCallback = (con: Connection, query: string, params: any[], ok: boolean, result: Error|pg.QueryResult, time: number) => void;
+export type EndCallback = (con: Connection, err?: Error, end?: boolean) => void;
 
 /**
  * A savepoint genereated from a transaction.
@@ -68,37 +113,70 @@ export interface SavePoint {
 
 export { QueryResult } from 'pg';
 
+const __onenhance: ConnectionCallback[] = [];
+export function onEnhance(run: ConnectionCallback): HookHandle {
+  __onenhance.push(run);
+  return {
+    cancel() {
+      const idx = __onenhance.indexOf(run);
+      if (!idx) __onenhance.splice(idx, 1);
+    }
+  };
+}
+
+const __onend: EndCallback[] = [];
+export function onEnd(run: EndCallback): HookHandle {
+  __onend.push(run);
+  return {
+    cancel() {
+      const idx = __onend.indexOf(run);
+      if (!idx) __onend.splice(idx, 1);
+    }
+  };
+}
+
+const __onresult: ResultCallback[] = [];
+export function onResult(run: ResultCallback): HookHandle {
+  __onresult.push(run);
+  return {
+    cancel() {
+      const idx = __onresult.indexOf(run);
+      if (!idx) __onresult.splice(idx, 1);
+    }
+  };
+}
+
+let count = 0;
+
 /**
  * Enhances the given pg.Client with a few convenience methods for managing
  * transactions and a tagged template helper for executing a query with
  * interpolations as parameters.
  */
 export function enhance(client: pg.Client|pg.ClientConfig): Connection {
-  if ('ts-pg-dao' in client) return client as Connection;
+  if (cfgprop in client) return client as Connection;
   if (!('connect' in client)) client = new pg.Client(client);
-  const res = client as any;
-  const config: {
-    attachQueryToError?: boolean;
-    attachParametersToError?: boolean;
-    clientStack?: boolean;
-  } = res['ts-pg-dao'] = { attachQueryToError: true, clientStack: true };
+  const db = client as any;
+  const config: InternalInfo = db[cfgprop] = { id: count++, attachQueryToError: true, clientStack: true };
 
-  res.inTransaction = false;
+  db.inTransaction = false;
 
-  res.begin = begin;
-  res.savepoint = savepoint;
-  res.rollback = rollback;
-  res.commit = commit;
-  res.transact = transact;
-  res.sql = sql;
-  res.lit = lit;
-  res.onCommit = onCommit;
-  res.onRollback = onRollback;
+  db.begin = begin;
+  db.savepoint = savepoint;
+  db.rollback = rollback;
+  db.commit = commit;
+  db.transact = transact;
+  db.sql = sql;
+  db.lit = lit;
+  db.onCommit = onCommit;
+  db.onRollback = onRollback;
+  db.onResult = _onResult;
+  db.onEnd = _onEnd;
 
-  const query = res.query;
-  let lastq: string;
-  let lastp: any[];
-  res.query = function(...args: any[]):any {
+  const query = db.query;
+  db.query = function(...args: any[]):any {
+    let lastq: string;
+    let lastp: any[];
     if (typeof args[0] === 'string') {
       lastq = args[0];
       if (Array.isArray(args[1])) lastp = args[1];
@@ -108,34 +186,70 @@ export function enhance(client: pg.Client|pg.ClientConfig): Connection {
     }
 
     const stack = config.clientStack ? new Error() : null;
+    const start = Date.now();
 
-    if (typeof args[args.length - 1] === 'function') {
-      query.apply(res, args.slice(0, -1).concat([(err: any, res: any) => {
-        if (err && lastq) {
+    const cb = typeof args[args.length - 1] === 'function' ? args.pop() : null;
+
+    const res = new Promise((ok, fail) => {
+      query.apply(db, args.concat([(err: any, res: any) => {
+        if (err && lastq && typeof err === 'object') {
           if (config.attachQueryToError) err.query = lastq;
-          if (config.attachParametersToError) err.parameters = lastp;
+          if (config.attachParametersToError) err.parameters = lastq;
         }
-        if (err && stack) err.stack = `${stack.stack}\n--- Driver exception:\n${err.stack}`
-        lastq = lastp = null;
-        args[args.length - 1](err, res);
+        if (err && stack && typeof err === 'object') err.stack = `${stack.stack}\n--- Driver exception:\n${err.stack}`
+        if (err) {
+          if (typeof err === 'object' && (config.__onresult || __onresult.length)) setTimeout(fireResult, 0, db, lastq || 'unknown', lastp || [], false, err, Date.now() - start);
+          fail(err);
+        } else {
+          if (config.__onresult || __onresult.length) setTimeout(fireResult, 0, db, lastq || 'unknown', lastp || [], true, res, Date.now() - start);
+          ok(res);
+        }
+        if (cb) cb(err, res);
       }]));
-    } else {
-      return new Promise((ok, fail) => {
-        query.apply(res, args.concat([(err: any, res: any) => {
-          if (err && lastq) {
-            if (config.attachQueryToError) err.query = lastq;
-            if (config.attachParametersToError) err.parameters = lastq;
-          }
-          if (err && stack) err.stack = `${stack.stack}\n--- Driver exception:\n${err.stack}`
-          lastq = lastp = null;
+    });
+
+    if (!cb) return res;
+  }
+
+  const end = db.end;
+  if (end) {
+    db.end = function(cb?: any) {
+      const stack = config.clientStack ? new Error() : null;
+      const res = new Promise<void>((ok, fail) => {
+        end.call(db, (err: Error) => {
+          if (err && stack && typeof err === 'object') err.stack = `${stack.stack}\n--- Driver exception:\n${err.stack}`
+
           if (err) fail(err);
-          else ok(res);
-        }]));
+          else ok();
+
+          if (cb) cb(err);
+
+          if (config.__onend) for (const cb of config.__onend) try { cb(db, err, true); } catch {}
+          if (__onend.length) for (const cb of __onend) try { cb(db, err, true); } catch {}
+          config.__onend = config.__onresult = config.__oncommit = config.__onrollback = undefined;
+        });
       });
+      if (!cb) return res;
     }
   }
 
-  return res as Connection;
+  const release = db.release;
+  if (release) {
+    db.release = function(err?: Error|boolean) {
+      release.call(db, err);
+      if (config.__onend || __onend.length) {
+        const stack = config.clientStack ? new Error() : null;
+        if (err && stack && typeof err === 'object' && 'stack' in err) stack.stack = `${err.stack}\n--- Release called from:\n${stack.stack}`;
+        if (config.__onend) for (const cb of config.__onend) try { cb(db, typeof err === 'boolean' ? (err ? stack : undefined) : err); } catch {}
+        for (const cb of __onend) try { cb(db); } catch {}
+        config.__onend = config.__onresult = config.__oncommit = config.__onrollback = undefined;
+      }
+    }
+  }
+
+  if (__onenhance) for (const cb of __onenhance) try { cb(db); } catch {}
+
+  return db as Connection;
 }
 
 async function begin(this: Connection) {
@@ -152,24 +266,26 @@ async function savepoint(this: Connection): Promise<SavePoint> {
   return { point };
 }
 
-async function rollback(this: Connection, point?: SavePoint) {
+async function rollback(this: Connection, point?: SavePoint, err?: Error) {
   if (!this.inTransaction) throw new Error(`Can't rollback when not in transaction`);
+  const t = this[cfgprop] as InternalInfo;
 
   if (point) {
     try {
       await this.query(`rollback to ${point.point}`);
+      // process rollback callbacks
+      if (Array.isArray(t.__onrollback)) for (const r of t.__onrollback) try { r(this, err, point); } catch {}
       return;
-    } catch {}
+    } catch (e) {
+      await this.rollback(undefined, e);
+    }
   }
+
   await this.query('rollback');
-  const t = this as any;
   // process rollback callbacks
-  if (Array.isArray(t.__rruns)) {
-    for (const r of t.__rruns) try { await r(); } catch {}
-    t.__rruns = [];
-  }
-  // discard commit callbacks
-  if (Array.isArray(t.__cruns)) t.__cruns = [];
+  if (Array.isArray(t.__onrollback)) for (const r of t.__onrollback) try { r(this, err); } catch {}
+  // discard callbacks
+  t.__oncommit = t.__onrollback = undefined;
   this.inTransaction = false;
 }
 
@@ -178,12 +294,9 @@ async function commit(this: Connection) {
   await this.query('commit');
   const t = this as any;
   // process commit callbacks
-  if (Array.isArray(t.__cruns)) {
-    for (const r of t.__cruns) try { await r(); } catch {}
-    t.__cruns = [];
-  }
-  // discard rollback callbacks
-  if (Array.isArray(t.__rruns)) t.__rruns = [];
+  if (Array.isArray(t.__oncommit)) for (const r of t.__oncommit) try { r(); } catch {}
+  // discard callbacks
+  t.__oncommit = t.__onrollback = undefined;
   this.inTransaction = false;
 }
 
@@ -195,7 +308,7 @@ async function transact<T>(this: Connection, cb: (con: Connection) => Promise<T>
     if (init) await this.commit();
     return res;
   } catch (ex) {
-    if (init) await this.rollback();
+    if (init) await this.rollback(undefined, ex);
     throw ex;
   }
 }
@@ -215,19 +328,51 @@ async function sql(this: Connection, strings: TemplateStringsArray, ...parts: an
   return this.query(sql, params);
 }
 
-async function onCommit(this: Connection, run: () => void|Promise<void>): Promise<void> {
-  if (!this.inTransaction) return run();
+function onCommit(this: Connection, run: ConnectionCallback): void {
+  if (!this.inTransaction) return run(this);
   else {
-    const t = this as any;
-    (t.__cruns || (t.__cruns = [])).push(run);
+    const t = this[cfgprop] as InternalInfo;
+    (t.__oncommit || (t.__oncommit = [])).push(run);
   }
 }
 
-async function onRollback(this: Connection, run: () => void|Promise<void>): Promise<void> {
+function onRollback(this: Connection, run: ConnectionCallback): void {
   if (!this.inTransaction) return;
   else {
-    const t = this as any;
-    (t.__rruns || (t.__rruns = [])).push(run);
+    const t = this[cfgprop] as InternalInfo;
+    (t.__onrollback || (t.__onrollback = [])).push(run);
+  }
+}
+
+function _onResult(this: Connection, run: ResultCallback): HookHandle {
+  const t = this[cfgprop] as InternalInfo;
+  (t.__onresult || (t.__onresult = [])).push(run);
+  return {
+    cancel() {
+      if (!t.__onresult) return;
+      const idx = t.__onresult.indexOf(run);
+      if (!idx) t.__onresult.splice(idx, 1);
+      if (t.__onresult.length === 0) t.__onresult = undefined;
+    }
+  }
+}
+
+function fireResult(con: Connection, query: string, params: any[], ok: boolean, result: Error|pg.QueryResult, time?: number) {
+  const t = con[cfgprop] as InternalInfo;
+  if (t && t.__onresult) for (const c of t.__onresult) try { c(con, query, params, ok, result, time); } catch {}
+  if (__onresult.length) for (const c of __onresult) try { c(con, query, params, ok, result, time); } catch {}
+}
+
+function _onEnd(this: Connection, run: EndCallback): HookHandle {
+  const t = this[cfgprop] as InternalInfo;
+  (t.__onend || (t.__onend = [])).push(run);
+  return {
+    cancel() {
+      if (!t.__onend) return;
+      const idx = t.__onend.indexOf(run);
+      if (!idx) t.__onend.splice(idx, 1);
+      if (t.__onend.length === 0) t.__onend = undefined;
+    }
   }
 }
 
